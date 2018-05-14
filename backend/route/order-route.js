17d0c334ad900dcf2d4258b76956ea9c2691cc3b
Oláh Péter Ciprián <peterciprian@gmail.com>
2018-05-14 15:33:03 +0200

    order router létrehozva

---
+++ backend/route/order-route.js
@@ -0,0 +1,11 @@
+const orderRouter = require('express').Router();
+const OrderController = require('../controller/order.controller');
+const permission = require('permission');
+
+orderRouter.get('/', permission(), OrderController.list);
+orderRouter.get('/:id', permission(), OrderController.getAllFromUser);
+orderRouter.post('/', permission(/*['admin']*/), OrderController.create);
+orderRouter.put('/:id', permission(/*['admin']*/), OrderController.update);
+orderRouter.put('/:id', permission(/*['admin']*/), OrderController.delete);
+
+module.exports = orderRouter;
