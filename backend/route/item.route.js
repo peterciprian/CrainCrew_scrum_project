384349d5f708b3ea4bb9965172c39ba4054f9222
Szász Kinga <kingus77@gmail.com>
2018-05-14 16:27:30 +0200

    small fixes

--- backend/route/item.route.js
+++ backend/route/item.route.js
@@ -6,6 +6,6 @@ itemRouter.get('/', permission(), ItemController.list);
 itemRouter.get('/:id', permission(), ItemController.find);
 itemRouter.post('/', permission(/*['admin']*/), ItemController.create);
 itemRouter.put('/:id', permission(/*['admin']*/), ItemController.update);
-itemRouter.delete('/:id', permission(/*['admin']*/), ItemController.remove);
+itemRouter.delete('/:id', permission(/*['admin']*/), ItemController.delete);
 
 module.exports = itemRouter;
