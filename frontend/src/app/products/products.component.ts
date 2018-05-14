2cad21cf710ae7a8a5dfc8d5910640c072b27328
Zsombor <bolezsombor@gmail.com>
2018-05-14 17:09:56 +0200

    tbc

--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
- // ebből kapjuk meg elvileg az items táblánkat, ráadásul JSON-ben:
- // 'mongoexport -h ds119150.mlab.com:19150 -d crane-crew -c items -u crane-crew -p crane-crew -o items.json';
-  constructor() { }
+ // ebből kapjuk meg elvileg az items táblánkat, ráadásul JSON-ben
+ // 'mongoexport -h ds119150.mlab.com:19150 -d crane-crew -c items -u crane-crew -p crane-crew -o items.json'
+ // csak még nem jöttem rá hogyan, lehet kell valami npm csomag hozzá
+ // (nem találtam olyat, aminek a használatatát is értettem volna)
+ constructor() {}
 
   ngOnInit() {
   }
