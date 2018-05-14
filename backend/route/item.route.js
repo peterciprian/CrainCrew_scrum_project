197b82afed7d942f27da27b24ca79844cb3aadb7
Oláh Péter Ciprián <peterciprian@gmail.com>
2018-05-14 14:08:10 +0200

    item.route szerkesztve, permission nélkül

--- backend/route/item.route.js
+++ backend/route/item.route.js
@@ -2,9 +2,10 @@ const itemRouter = require('express').Router();
 const ItemController = require('../controller/item.controller');
 const permission = require('permission');
 
-itemRouter.get('/profile', permission(), ItemController.profile);
-itemRouter.post('/register', ItemController.register);
-itemRouter.post('/login', ItemController.login);
-itemRouter.get('/logout', ItemController.logout);
+itemRouter.get('/', permission(), ItemController.list);
+itemRouter.get('/:id', permission(), ItemController.find);
+itemRouter.post('/', permission(/*['admin']*/), ItemController.create);
+itemRouter.put('/:id', permission(/*['admin']*/), ItemController.update);
+itemRouter.delete('/:id', permission(/*['admin']*/), ItemController.delete);
 
 module.exports = itemRouter;
