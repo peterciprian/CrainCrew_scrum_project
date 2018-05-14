1f05ede0d7d2ff1754f8ac9518d56e22c8656b08
Zsombor <bolezsombor@gmail.com>
2018-05-14 16:38:39 +0200

    item táblához link

--- backend/route/item.route.js
+++ backend/route/item.route.js
@@ -4,7 +4,7 @@ const permission = require('permission');
 
 itemRouter.get('/', permission(), ItemController.list);
 itemRouter.get('/:id', permission(), ItemController.find);
-itemRouter.post('/', permission(/*['admin']*/), ItemController.create);
+itemRouter.post('/', ItemController.create);
 itemRouter.put('/:id', permission(/*['admin']*/), ItemController.update);
 itemRouter.delete('/:id', permission(/*['admin']*/), ItemController.delete);
 
--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+ // ebből kapjuk meg elvileg az items táblánkat, ráadásul JSON-ben:
+ // 'mongoexport -h ds119150.mlab.com:19150 -d crane-crew -c items -u crane-crew -p crane-crew -o items.json';
   constructor() { }
 
   ngOnInit() {
