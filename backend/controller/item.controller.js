e217fcc49a909cfc17603d4663541eec0001d5c7
Zsombor <bolezsombor@gmail.com>
2018-05-14 13:46:40 +0200

    item controller

---
+++ backend/controller/item.controller.js
@@ -0,0 +1,33 @@
+const Item = require('../models/item-model.js');
+const mongoose = require('mongoose');
+
+module.exports = {
+  list: (req, res) => {
+    Item.find({}).then(item => res.json(item))
+      .catch(err => res.send(err));
+  },
+
+  find: (req, res) => {
+    Item.findById(req.params.id)
+      .then(item => res.json(item))
+      .catch(err => res.send(err));
+  },
+
+  create: (req, res) => {
+    Item.create(req.body)
+      .then(item => res.send(item))
+      .catch(err => res.send(err));
+  },
+
+  update: (req, res) => {
+    Item.findByIdAndUpdate(req.params.id, req.body)
+      .then(item => res.json(item))
+      .catch(err => res.send(err));
+  },
+
+  remove: (req, res) => {
+    Item.findByIdAndRemove(req.params.id)
+      .then(item => res.json(item))
+      .catch(err => res.send(err));
+  },
+};
