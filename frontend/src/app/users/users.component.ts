46dc819a742f7ae2aba4263996ca6457a310259f
ozsiga <oberthzsigmond@gmail.com>
2018-05-15 12:30:45 +0200

    frontend crud for users

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  options = new RequestOptions({ withCredentials: true });
+  baseUrl = 'http://localhost:8080/user/users';
+  users: any;
+  selectedUser: any;
+  constructor(public http: Http) {this.getUsers()}
 
   ngOnInit() {
+  }
+
+  getUsers() {
+    this.http.get(this.baseUrl, this.options)
+      .subscribe(data => {
+        this.users = JSON.parse(data['_body']);
+        console.log(this.users);
+  });
+  }
+
+  editUser(user) {
+    this.selectedUser = user;
+    this.http.put(`http://localhost:8080/user/update/${this.selectedUser['_id']}`, this.selectedUser, this.options)
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
+  removeUser(user) {
+      this.http.delete(`http://localhost:8080/user/${user['_id']}`, this.options)
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 
 }
