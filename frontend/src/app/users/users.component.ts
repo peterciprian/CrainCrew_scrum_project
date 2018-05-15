def6d988f592efe72207dda022c9d8e257c13068
Szász Kinga <kingus77@gmail.com>
2018-05-15 11:49:52 +0200

    user.components change

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: Array<any>;
 
-  constructor() { }
+  user = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  modal:{
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  url = 'http://localhost:8080/user/';
+
+  constructor(public http: HttpClient) {
+    this.getAll();
+  }
+
+  // feltölti a entries változót a szerverről kapott adatokkal
+  getAll() {
+    this.http.get(this.url + 'users/').subscribe(
+      (data: any) => {
+        this.users = data;
+        console.log(this.users);
+      }
+    );
+  }
+
+  
+  modalChange(id) {
+    const choosen = this.users.filter(user => user._id === id)[0];
+    this.modal = Object.assign({}, choosen); 
+  }
+
+  delete(id) {
+    if (confirm('Really?')) {
+      this.http.delete(this.url + id)
+        .subscribe(
+          (data) => {
+            this.getAll();
+          }
+        );
+    }
+  }
 
   ngOnInit() {
   }
-
 }
