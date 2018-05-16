86cefbbf825092c3555e522136e2624becd147de
Szász Kinga <kingus77@gmail.com>
2018-05-16 16:47:19 +0200

    sync

--- backend/route/order.route.js
+++ backend/route/order.route.js
@@ -3,11 +3,7 @@ const OrderController = require('../controller/order.controller');
 const permission = require('permission');
 
 orderRouter.get('/', permission(), OrderController.list);
-<<<<<<< HEAD
-//orderRouter.get('/:id', permission(), OrderController.getAllFromUser);
-=======
 orderRouter.get('/:id', permission(), OrderController.find);
->>>>>>> cedc0ef26158df26d9dfff7a3a28c9271df02ed8
 orderRouter.post('/', permission('admin'), OrderController.create);
 orderRouter.put('/update/:id', permission('admin'), OrderController.update);
 orderRouter.delete('/delete/:id', permission('admin'), OrderController.delete);
