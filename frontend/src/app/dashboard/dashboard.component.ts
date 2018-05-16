5d2b546b09ddfbaf82f33e73507b58ca068b884a
ozsiga <oberthzsigmond@gmail.com>
2018-05-16 04:17:27 +0200

    google chart

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
+import { ProductsComponent } from '../products/products.component';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
+  a = Math.floor(Math.random() * Math.floor(200));
+  b = Math.floor(Math.random() * Math.floor(200));
+  c = Math.floor(Math.random() * Math.floor(200));
+  d = Math.floor(Math.random() * Math.floor(200));
+  productcat: any;
+  options = new RequestOptions({ withCredentials: true });
+  income: number;
+  pieChartData = {
+    chartType: 'ColumnChart',
+    dataTable: [
+      ['0', '0'],
+      ['Felhasználók', this.a],
+      ['Eladott termék', this.b],
+      ['Termék ára', this.d],
+      ['Nem vásárló felhasználók', (this.b - this.a) * (-1)],
+      ['Átlag', (this.c * this.d) / this.b]
+    ],
+    options: {
+      'title': 'Stat',
+      legend: 'none'
+    },
+  };
+  constructor(public http: Http) {
+    this.getProducts();
+   }
   ngOnInit() {
   }
-
+  getProducts() {
+    this.http.get('http://localhost:8080/item', this.options)
+      .subscribe(getProducts => {
+        this.productcat = JSON.parse(getProducts['_body']);
+        console.log(this.productcat);
+        this.Income();
+      });
+  }
+  Income() {
+    this.income = 0;
+    for (let i = 0; i < (this.productcat).length; i++) {
+      this.income += ((this.productcat)[i]).price;
+    }
+    console.log(this.income);
+  }
 }
