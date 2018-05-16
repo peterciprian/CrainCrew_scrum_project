bb2fc6f4b4aaf4e035c8a3bfd8022da069e32fb3
ozsiga <oberthzsigmond@gmail.com>
2018-05-16 13:04:00 +0200

    chart kész ha igaz

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, NgModule } from '@angular/core';
 import { Http, RequestOptions } from '@angular/http';
 import { OrdersComponent } from '../orders/orders.component';
 import { Observable } from 'rxjs/Observable';
-import { UsersComponent } from '../users/users.component';
-
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,33 +10,35 @@ import { UsersComponent } from '../users/users.component';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  a = Math.floor(Math.random() * Math.floor(200));
-  b = Math.floor(Math.random() * Math.floor(200));
-  c = Math.floor(Math.random() * Math.floor(200));
-  d = Math.floor(Math.random() * Math.floor(200));
   order: any;
   options = new RequestOptions({ withCredentials: true });
   orderPrice: number;
+  singleorderPrice: Array<number>;
+  singleorderDate: Array<number>;
   orderDate: string;
+  filltable: boolean;
   pieChartData = {
-    chartType: 'LineChart',
+    chartType: 'ColumnChart',
     dataTable: [
       ['0', '0'],
-      ['date', this.orderPrice],
     ],
     options: {
       'title': 'E havi bevétel napokra bontva',
-      legend: 'none'
+      legend: 'none',
+      is3D: 'true',
     },
   };
   constructor(public http: Http) {
     this.getOrders();
     setTimeout(() => {
       this.getAllOrdersPrice();
-    }, 5000);
+    }, 3000);
     setTimeout(() => {
       this.getOrdersDate();
-    }, 5000);
+    }, 3000);
+     setTimeout(() => {
+      this.fillDataTable();
+    }, 3000);
    }
   ngOnInit() {
   }
@@ -50,19 +51,31 @@ export class DashboardComponent implements OnInit {
   }
   getAllOrdersPrice() {
     this.orderPrice = 0;
+    this.singleorderPrice = [];
     for (let i = 0; i < this.order.length; i++) {
       this.orderPrice += this.order[i].price;
+      this.singleorderPrice.push(Number(this.order[i].price));
     }
     console.log(this.orderPrice);
+    console.log(this.singleorderPrice);
   }
   getOrdersDate() {
-    this.orderDate = '';
-    for (let i = 0; i < (this.order).length; i++) {
-      this.orderDate += ((this.order)[i]).createdAt.slice(5, 10);
+    this.singleorderDate = Array<number>;
+    for (let i = 0; i < this.order.length; i++) {
+      this.singleorderDate.push(Number(this.order[i].createdAt.slice(8, 10)));
      }
-     console.log(this.orderDate);
+     console.log(this.singleorderDate);
   }
 
-
+  fillDataTable() {
+    for (let i = 0; i < this.singleorderPrice.length; i++) { 
+      this.pieChartData.dataTable.push([this.singleorderDate[i], this.singleorderPrice[i]]);
+      this.filltable = true;
+  }
+    console.log(this.pieChartData);
+  }
 }
+
+
+
 
