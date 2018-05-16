b8653ec2142f4e5a02582e4170a159560739aa50
Oláh Péter Ciprián <peterciprian@gmail.com>
2018-05-16 14:22:02 +0200

    order component CRUDtable kész (igen, KÉSZ!!)

--- frontend/src/app/orders/orders.component.ts
+++ frontend/src/app/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestOptions, Http } from '@angular/http';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  options = new RequestOptions({ withCredentials: true });
+  baseUrl = 'http://localhost:8080/item/';
+
+  orders: any;
+  users: any;
+  items: any;
+
+  actualOrder: object = {
+    user : '',
+    items : [
+      {item : '', quantity : ''}
+    ],
+    price : ''
+  };
+
+  newOrder: object = {
+    user : '',
+    items : [
+      {item : '', quantity : ''}
+    ],
+    price : ''
+  };
+
+
+  constructor(public http: Http) {
+    this.listOrders();
+    this.listItems();
+    this.listUsers();
+  }
 
   ngOnInit() {
+  }
+
+  listOrders() {
+    this.http.get(this.baseUrl, this.options)
+      .subscribe(data => {
+        this.orders = JSON.parse(data['_body']);
+      });
+  }
+  listUsers() {
+    this.http.get('http://localhost:8080/user/users', this.options)
+      .subscribe(data => {
+        this.users = JSON.parse(data['_body']);
+      });
+  }
+  listItems() {
+    this.http.get('http://localhost:8080/item/', this.options)
+      .subscribe(data => {
+        this.items = JSON.parse(data['_body']);
+      });
+  }
+
+  find(orderId) {
+    this.http.get(this.baseUrl + orderId, this.options)
+      .subscribe(data => {
+        this.orders = JSON.parse(data['_body']);
+        console.log(this.orders);
+      });
+  }
+
+  create() {
+    console.log(this.newOrder);
+    this.http.post(this.baseUrl, this.newOrder, this.options)
+      .subscribe(data => {
+        console.log(data);
+        this.listOrders();
+      });
+  }
+  update(order) {
+    this.actualOrder = order;
+    this.http.put(this.baseUrl + 'update/' + this.actualOrder['_id'], this.actualOrder, this.options)
+      .subscribe(data => {
+        console.log(data);
+        this.listOrders();
+      });
+  }
+
+  delete(itemId) {
+    if (confirm('Really?')) {
+      this.http.delete(this.baseUrl + 'delete/' + itemId, this.options)
+        .subscribe(data => {
+          console.log(data);
+          this.listOrders();
+        });
+    }
   }
 
 }
