3a764bcdf027c95882a57e02fdd7373388601a8e
Szász Kinga <kingus77@gmail.com>
2018-05-20 14:48:10 +0200

    Feat:public item page - basic

--- backend/route/item.route.js
+++ backend/route/item.route.js
@@ -2,7 +2,7 @@ const itemRouter = require('express').Router();
 const ItemController = require('../controller/item.controller');
 const permission = require('permission');
 
-itemRouter.get('/', permission(), ItemController.list);
+itemRouter.get('/', ItemController.list);
 itemRouter.get('/:id', permission(), ItemController.find);
 itemRouter.post('/', permission('admin'), ItemController.create);
 itemRouter.put('/:id', permission('admin'), ItemController.update);
