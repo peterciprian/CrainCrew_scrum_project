359df68038ebcd5b8bd2e597ebb89b35420becb7
Oláh Péter Ciprián <peterciprian@gmail.com>
2018-05-21 19:57:43 +0200

    checkout loadorder megírva, nincs tesztelve, holnap folytatom navbar component tslint hibák javítva

--- frontend/src/app/checkout/checkout.component.ts
+++ frontend/src/app/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { RequestOptions, Http } from '@angular/http';
+import { FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -9,10 +11,35 @@ import { FlashMessagesService } from 'angular2-flash-messages';
 export class CheckoutComponent implements OnInit {
 
   public cart = JSON.parse(localStorage.getItem('cartItems'));
+  options = new RequestOptions({ withCredentials: true });
+  baseUrl = 'http://localhost:8080/order/';
+  newOrder = {
+    user: '',
+    items: [
+      { item: '', quantity: 1 }
+    ],
+    price: 0
+  };
+  loggedInUser: any;
 
-  constructor() { }
+  constructor(public http: Http) { }
 
   ngOnInit() {
+    this.loadOrder();
+  }
+
+  loadOrder() {
+    this.http.get('http://localhost:8080/user/profile', this.options)
+      .subscribe(data => {
+        this.loggedInUser = JSON.parse(data['_body']);
+        this.newOrder.user = this.loggedInUser.user['_id'];
+      });
+    this.newOrder.price = this.cartSum();
+    this.newOrder.items = [];
+    for (let i = 0; i < this.cart.length; i++) {
+      this.newOrder.items.push({item: this.cart[i]['_id'], quantity: 1});
+    }
+    console.log(this.newOrder);
   }
 
   cartSum() {
@@ -21,6 +48,14 @@ export class CheckoutComponent implements OnInit {
       sum += item.price;
     });
     return sum;
+  }
+
+  createOrder(cart) {
+    console.log(this.newOrder);
+    /*this.http.post(this.baseUrl, this.newOrder, this.options)
+      .subscribe(data => {
+        console.log(data);
+      });*/
   }
 
 }
