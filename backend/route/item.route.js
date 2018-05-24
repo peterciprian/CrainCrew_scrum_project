bb89e30a6534a7a728f2d790c3807dff817f9d7b
Zsombor <bolezsombor@gmail.com>
2018-05-24 09:57:19 +0200

    permission fix

--- backend/route/item.route.js
+++ backend/route/item.route.js
@@ -3,7 +3,7 @@ const ItemController = require('../controller/item.controller');
 const permission = require('permission');
 
 itemRouter.get('/', ItemController.list);
-itemRouter.get('/:id', permission(), ItemController.find);
+itemRouter.get('/:id', ItemController.find);
 itemRouter.post('/', permission('admin'), ItemController.create);
 itemRouter.put('/:id', permission('admin'), ItemController.update);
 itemRouter.delete('/:id', permission('admin'), ItemController.delete);
